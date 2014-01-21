60f22dceebcde895d2b9783ab8a2ddc5a7b1b358
Eli Shkurkin <null>
2014-01-20 20:27:55 -0800

    User can create events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,11 @@
 class EventsController < ApplicationController
   def index
+    @event = Event.new
   end
+
+  def create
+    Event.create(params[:event])
+    redirect_to root_path
+  end
+
 end
