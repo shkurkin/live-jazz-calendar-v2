c8659407c5c94e3759fa39ee97ca288da339b805
Eli Shkurkin <null>
2014-01-20 19:14:46 -0800

    Boilerplate Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiveJazzCalendarV2::Application.config.secret_token = 'f814656eba75f63786baeb79cff3a3f0b24a4ff28a59824194a2749638fd119a496ef508b56d627451dddb8c801352d7fdb1bc807c2ba3c904662c0b7a4da39c'
